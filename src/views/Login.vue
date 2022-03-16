3cb29fd6c4cc972e33cf3dde8fb07416c17c23a9
StarkOne <stark.pro40@gmail.com>
2022-03-16 14:22:42 +0700

    add login module

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,9 +3,9 @@
         <div class="container page">
             <div class="row">
                 <div class="col-md-6 offset-md-3 col-xs-12">
-                    <h1 class="text-xs-center">Sign up</h1>
+                    <h1 class="text-xs-center">Sign in</h1>
                     <p class="text-xs-center">
-                        <router-link :to="{ name: 'login' }">Have an account?</router-link>
+                        <router-link :to="{ name: 'register' }">Need an account?</router-link>
                     </p>
                     <mcv-validation-errors v-if="validationErrors" :validation-errors="validationErrors" />
                     <form @submit.prevent="onSubmit">
@@ -19,14 +19,6 @@
                         </fieldset>
                         <fieldset class="form-group">
                             <input
-                                v-model="username"
-                                type="text"
-                                class="form-control form-control-lg"
-                                placeholder="Username"
-                            />
-                        </fieldset>
-                        <fieldset class="form-group">
-                            <input
                                 v-model="password"
                                 type="password"
                                 autocomplete="on"
@@ -34,7 +26,7 @@
                                 placeholder="Password"
                             />
                         </fieldset>
-                        <button :disabled="isSubmitting" class="btn btn-lg btn-primary pull-xs-right">Sign Up</button>
+                        <button :disabled="isSubmitting" class="btn btn-lg btn-primary pull-xs-right">Sign In</button>
                     </form>
                 </div>
             </div>
@@ -43,19 +35,19 @@
 </template>
 
 <script>
-import McvValidationErrors from '@/components/ValidationErrors.vue';
-import { actionTypes } from '@/store/modules/auth';
 import { mapState } from 'vuex';
 
+import McvValidationErrors from '@/components/ValidationErrors.vue';
+import { actionTypes } from '@/store/modules/auth';
+
 export default {
-    name: 'McvRegister',
+    name: 'McvLogin',
     components: {
         McvValidationErrors,
     },
     data() {
         return {
             email: '',
-            username: '',
             password: '',
         };
     },
@@ -68,9 +60,8 @@ export default {
     methods: {
         onSubmit() {
             this.$store
-                .dispatch(actionTypes.register, {
+                .dispatch(actionTypes.login, {
                     email: this.email,
-                    username: this.username,
                     password: this.password,
                 })
                 .then(() => {
